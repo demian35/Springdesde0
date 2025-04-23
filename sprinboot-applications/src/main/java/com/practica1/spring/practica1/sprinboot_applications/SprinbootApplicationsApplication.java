fd3374d186fa7eb3fd9dc7af9733c80733fef0f4
demian35 <josedemian@ciencias.unam.mx>
2025-04-22 18:04:39 -0600

    creando el proyecto para la practica 1

---
+++ sprinboot-applications/src/main/java/com/practica1/spring/practica1/sprinboot_applications/SprinbootApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.practica1.spring.practica1.sprinboot_applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootApplicationsApplication.class, args);
+	}
+
+}
---
+++ sprinboot-applications/src/test/java/com/practica1/spring/practica1/sprinboot_applications/SprinbootApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica1.spring.practica1.sprinboot_applications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
