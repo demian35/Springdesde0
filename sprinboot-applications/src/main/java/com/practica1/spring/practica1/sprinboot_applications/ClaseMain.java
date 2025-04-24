20b3b2c108cdae46699837c5bbc2ec35237f97f2
demian35 <josedemian@ciencias.unam.mx>
2025-04-24 17:14:25 -0600

    creando la ejecucion de la inyeccion automatica

--- sprinboot-applications/src/main/java/com/practica1/spring/practica1/sprinboot_applications/ClaseMain.java
+++ sprinboot-applications/src/main/java/com/practica1/spring/practica1/sprinboot_applications/ClaseMain.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import com.practica1.spring.practica1.sprinboot_applications.inyeccionautomatica.Myservice;
+
 import jakarta.annotation.PostConstruct;
 
 /**Clase que ejecutara el bean MiPrimerBean */
@@ -14,9 +16,12 @@ public class ClaseMain {
     @Autowired
     private ApplicationContext ap;
 
+    @Autowired
+    private Myservice servicio;
+
     //metodo que se ejecutara cuando se cree el bean y mostrara los beans creados
     @PostConstruct
     public void init(){
-        System.out.println("algo");
+        servicio.ejecutaOperacion();
     }
 }
