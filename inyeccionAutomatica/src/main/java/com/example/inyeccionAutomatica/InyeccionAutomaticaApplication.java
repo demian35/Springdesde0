846c327219107d9a9155b88821d91ae2767b8967
demian35 <josedemian@ciencias.unam.mx>
2025-04-24 16:27:07 -0600

    agregando proyecto para ejemplos de inyecciones automaticas

---
+++ inyeccionAutomatica/src/main/java/com/example/inyeccionAutomatica/InyeccionAutomaticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.inyeccionAutomatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyeccionAutomaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InyeccionAutomaticaApplication.class, args);
+	}
+
+}
---
+++ inyeccionAutomatica/src/test/java/com/example/inyeccionAutomatica/InyeccionAutomaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inyeccionAutomatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InyeccionAutomaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
