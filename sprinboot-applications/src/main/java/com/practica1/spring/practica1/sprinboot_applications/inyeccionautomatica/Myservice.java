951c0a584b773fb2a4c7d00cc32de7e437797b3c
demian35 <josedemian@ciencias.unam.mx>
2025-04-24 17:14:07 -0600

    corrigiendo error en el atributo repostory de Myservice, faltaba la anotacion @autowired

--- sprinboot-applications/src/main/java/com/practica1/spring/practica1/sprinboot_applications/inyeccionautomatica/Myservice.java
+++ sprinboot-applications/src/main/java/com/practica1/spring/practica1/sprinboot_applications/inyeccionautomatica/Myservice.java
@@ -1,10 +1,13 @@
 package com.practica1.spring.practica1.sprinboot_applications.inyeccionautomatica;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //clase que implementa la logica del proyecto (repository)
 @Service
 public class Myservice {
+
+    @Autowired
     private Myrepository repository;
 
     //metodo que ejecuta al repository
