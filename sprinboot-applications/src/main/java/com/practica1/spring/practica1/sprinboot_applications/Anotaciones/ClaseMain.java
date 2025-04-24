1cf0b03ac4c84591d5eaae06f3356b71eaf595d8
demian35 <josedemian@ciencias.unam.mx>
2025-04-24 14:41:47 -0600

    creando otro bean que imprimira los beans que se crean en la ejecucion del proyecto

---
+++ sprinboot-applications/src/main/java/com/practica1/spring/practica1/sprinboot_applications/Anotaciones/ClaseMain.java
@@ -0,0 +1,23 @@
+package com.practica1.spring.practica1.sprinboot_applications.Anotaciones;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+/**Clase que ejecutara el bean MiPrimerBean */
+@Component
+public class ClaseMain {
+
+    @Autowired
+    private ApplicationContext ap;
+
+    //metodo que se ejecutara cuando se cree el bean y mostrara los beans creados
+    @PostConstruct
+    public void init(){
+        for(String s :ap.getBeanDefinitionNames()){
+            System.out.println(">>>" + s);
+        }
+    }
+}
