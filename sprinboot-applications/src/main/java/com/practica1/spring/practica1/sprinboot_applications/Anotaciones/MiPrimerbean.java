de77972868b4cdd0e82ac970cede3e3d5266c246
demian35 <josedemian@ciencias.unam.mx>
2025-04-24 14:20:16 -0600

    creando un bean @Component

---
+++ sprinboot-applications/src/main/java/com/practica1/spring/practica1/sprinboot_applications/Anotaciones/MiPrimerbean.java
@@ -0,0 +1,10 @@
+package com.practica1.spring.practica1.sprinboot_applications.Anotaciones;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MiPrimerbean {
+
+    public MiPrimerbean(){}
+    
+}
