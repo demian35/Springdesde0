85fa5f6beb69c57014e828aefa13d5b669e0c281
demian35 <josedemian@ciencias.unam.mx>
2025-04-28 16:19:03 -0600

    uso de la notacion @Qualifier para usar un bean en especifico

--- anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/ClaseMaindementira.java
+++ anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/ClaseMaindementira.java
@@ -1,6 +1,7 @@
 package com.anotacionesbeans.anotacionesbeans;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.anotacionesbeans.anotacionesbeans.beans.Query;
@@ -13,6 +14,7 @@ public class ClaseMaindementira {
 
     //objeto query
     @Autowired
+    @Qualifier("fileQuery") //uso de la notacion @Qualifier para usar el bean fileQuery
     private Query query;
 
     //metodo que ejecuta una consulta de acceso a datos
