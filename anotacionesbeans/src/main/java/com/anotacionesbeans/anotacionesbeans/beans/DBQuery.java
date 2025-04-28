400b6556779fa5f019cab570b5c9a1fa03ed6b38
demian35 <josedemian@ciencias.unam.mx>
2025-04-28 13:22:33 -0600

    Clase que simula el acceso a datos de una base de datos

---
+++ anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/beans/DBQuery.java
@@ -0,0 +1,13 @@
+package com.anotacionesbeans.anotacionesbeans.beans;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DBQuery implements Query {
+
+    @Override
+    public List<
+    
+}
