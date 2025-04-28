13bdd15baac8e5742e4e149157301c43743b5c1a
demian35 <josedemian@ciencias.unam.mx>
2025-04-28 16:26:57 -0600

    asignando un valor personalizado al bean FileQuery

--- anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/beans/FileQuery.java
+++ anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/beans/FileQuery.java
@@ -2,15 +2,20 @@ package com.anotacionesbeans.anotacionesbeans.beans;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 //clase que nos regresa los elementos de un archivo
 @Component
 public class FileQuery implements Query {
 
+    //value notacion para dar valores personalizados a los beans
+    @Value("${query.file.message}")
+    private String message;
+
     @Override
     public List<String> muestraResultados(){
-        return List.of("elementos ", "de", "un " , "archivo");
+        return List.of(message);
     }
 
     
