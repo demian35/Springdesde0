3eb5bef1ca27b05a7567f12df3e9bd81ccdad5f5
demian35 <josedemian@ciencias.unam.mx>
2025-04-28 14:07:01 -0600

    agregando notaciones faltantes

--- anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/ClaseMaindementira.java
+++ anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/ClaseMaindementira.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 
 import com.anotacionesbeans.anotacionesbeans.beans.Query;
 
+import jakarta.annotation.PostConstruct;
+
 // clase que ejecuta la logica del proyecto
 @Component
 public class ClaseMaindementira {
@@ -14,6 +16,7 @@ public class ClaseMaindementira {
     private Query query;
 
     //metodo que ejecuta una consulta de acceso a datos
+    @PostConstruct
     public void ejecutar(){
         System.out.println(query.muestraResultados());
     }
