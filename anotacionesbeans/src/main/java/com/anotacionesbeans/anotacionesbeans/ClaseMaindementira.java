c930f99e34a678910bba15ca431d95266fe5acd8
demian35 <josedemian@ciencias.unam.mx>
2025-04-28 13:37:29 -0600

    clase que ejecuta el proyecto

--- anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/ClaseMaindementira.java
+++ anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/ClaseMaindementira.java
@@ -1,7 +1,22 @@
 package com.anotacionesbeans.anotacionesbeans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.anotacionesbeans.anotacionesbeans.beans.Query;
+
+// clase que ejecuta la logica del proyecto
+@Component
 public class ClaseMaindementira {
 
-    
+    //objeto query
+    @Autowired
+    private Query query;
+
+    //metodo que ejecuta una consulta de acceso a datos
+    public void ejecutar(){
+        System.out.println(query.muestraResultados());
+    }
+
     
 }
