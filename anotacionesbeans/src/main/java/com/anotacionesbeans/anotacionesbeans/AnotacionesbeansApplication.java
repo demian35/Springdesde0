2ec4569a8e98f4e04d33cf30d7264fb21cd2c54b
demian35 <josedemian@ciencias.unam.mx>
2025-04-28 12:30:03 -0600

    creando proyecto para practicar el manejo de anotaciones de beans

---
+++ anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/AnotacionesbeansApplication.java
@@ -0,0 +1,13 @@
+package com.anotacionesbeans.anotacionesbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotacionesbeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotacionesbeansApplication.class, args);
+	}
+
+}
---
+++ anotacionesbeans/src/test/java/com/anotacionesbeans/anotacionesbeans/AnotacionesbeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anotacionesbeans.anotacionesbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotacionesbeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
