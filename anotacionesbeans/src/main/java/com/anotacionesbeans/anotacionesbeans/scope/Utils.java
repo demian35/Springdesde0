675ce4af06b1eee1f9bf2c66f45114ffd42261cc
demian35 <josedemian@ciencias.unam.mx>
2025-04-29 13:07:31 -0600

    Arreglando error en @scope, es prototype en lugar de prototipe

--- anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/scope/Utils.java
+++ anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/scope/Utils.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototipe")
+@Scope("prototype")
 public class Utils {
     
 }
