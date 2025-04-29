41f22fdc17bea52aa5699e7ce2fe4d2648f281e9
demian35 <josedemian@ciencias.unam.mx>
2025-04-29 13:08:14 -0600

    creando e imprimiendo beans que creamos con el @scope en este caso Service1, 2 y 3

--- anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/ClaseMaindementira.java
+++ anotacionesbeans/src/main/java/com/anotacionesbeans/anotacionesbeans/ClaseMaindementira.java
@@ -1,10 +1,16 @@
 package com.anotacionesbeans.anotacionesbeans;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.anotacionesbeans.anotacionesbeans.beans.Query;
+import com.anotacionesbeans.anotacionesbeans.scope.Service1;
+import com.anotacionesbeans.anotacionesbeans.scope.Service2;
+import com.anotacionesbeans.anotacionesbeans.scope.Service3;
 
 import jakarta.annotation.PostConstruct;
 
@@ -17,10 +23,33 @@ public class ClaseMaindementira {
     @Qualifier("fileQuery") //uso de la notacion @Qualifier para usar el bean fileQuery
     private Query query;
 
+    @Autowired
+    private Service1 service1;
+
+    @Autowired 
+    private Service2 service2;
+
+    @Autowired
+    private Service3 service3;
+
+    @Autowired
+    private ApplicationContext ctx;
+    
+
     //metodo que ejecuta una consulta de acceso a datos
     @PostConstruct
     public void ejecutar(){
+
+        //for para imprmir todos los beans que se crean durante la ejecucion
+        for(String s: ctx.getBeanDefinitionNames() ){
+            System.out.println(">>>>" + s);
+        }
+
         System.out.println(query.muestraResultados());
+
+        System.out.println(service1.utils);
+        System.out.println(service2.utils);
+        System.out.println(service3.utils);
     }
 
     
