98ea9d1bff7761c0ab0eb3f835f6a7b66378286e
demian35 <josedemian@ciencias.unam.mx>
2025-04-29 19:00:23 -0600

    uso del objeto model para mostrar mensajes en pantalla

--- demoController/src/main/java/com/exampleController/demoController/Controlador.java
+++ demoController/src/main/java/com/exampleController/demoController/Controlador.java
@@ -1,7 +1,10 @@
 package com.exampleController.demoController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 //clase controlador que gestionara las peticiones y las respuestas del proyecto
 @Controller
@@ -12,6 +15,15 @@ public class Controlador {
     @GetMapping("/") //ejemplo http://localhost8080/
     public String pantallaPrincipal(){
         return "index";
+    }
+
+    /*medoto que redirecciona a otra pagina cuando se le pasa http://localhost8080/me 
+     * la diferencia es que se usa model, al html se le pasa el mensaje 
+     */
+    @GetMapping("/me")
+    public String perfil(Model model){
+        model.addAttribute("mensaje", "Bienvenido a mi perfil");
+        return "perfil";
     }
 
 }
