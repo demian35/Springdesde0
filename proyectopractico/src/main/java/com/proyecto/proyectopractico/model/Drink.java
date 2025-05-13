2644f9f71e9196c3bffe4a93b50c4c465d2a6b2b
demian35 <josedemian@ciencias.unam.mx>
2025-05-13 16:57:17 -0600

    modelo de la bebida creado

--- proyectopractico/src/main/java/com/proyecto/proyectopractico/model/Drink.java
+++ proyectopractico/src/main/java/com/proyecto/proyectopractico/model/Drink.java
@@ -1,9 +1,63 @@
 package com.proyecto.proyectopractico.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 //clase Drink que modelara un objeto bebida
 @Entity
 public class Drink {
     
+    //atributos del objeto bebida
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idBebida; //id de la bebida
+
+    private String name;
+    private String description;
+    private String image; //guardara la url de la imagen del trago}
+
+    public Drink(){}//Constructor sin argumentos
+
+    public Drink(String name,String description,String image){//constructor con argumentos
+        this.name=name;
+        this.description=description;
+        this.image=image;
+    }
+
+    //metodos get y set
+
+    public Long getID(){
+        return idBebida;
+    }
+
+    public void setID(Long idBebida){
+        this.idBebida=idBebida;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description=description;
+    }
+
+    public String getImage(){
+        return image;
+    }
+
+    public void setImage(String image){
+        this.image=image;
+    }
+
 }
