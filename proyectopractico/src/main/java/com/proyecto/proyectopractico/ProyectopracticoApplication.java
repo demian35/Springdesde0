5fabf93b6fc16733ce9e9232bf77be6b9b7bc118
demian35 <josedemian@ciencias.unam.mx>
2025-05-13 14:51:00 -0600

    creando y agregando dependencias para el pryecto de practica, menu de tragos

---
+++ proyectopractico/src/main/java/com/proyecto/proyectopractico/ProyectopracticoApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyectopractico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectopracticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectopracticoApplication.class, args);
+	}
+
+}
---
+++ proyectopractico/src/test/java/com/proyecto/proyectopractico/ProyectopracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyectopractico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectopracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
