9e62858a0df9b7d7fcebffef896ac6ea67efd149
demian35 <josedemian@ciencias.unam.mx>
2025-05-14 16:18:31 -0600

    metodo para realizar la busqueda por nombre de un trago

--- proyectopractico/src/main/java/com/proyecto/proyectopractico/repository/DrinkRepository.java
+++ proyectopractico/src/main/java/com/proyecto/proyectopractico/repository/DrinkRepository.java
@@ -1,5 +1,7 @@
 package com.proyecto.proyectopractico.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.proyecto.proyectopractico.model.Drink;
 //interfaz que nos ayudara con el acceso a las bebidas
 @Repository
 public interface DrinkRepository extends JpaRepository<Drink,Long> {
+
+    List<Drink> findByNameContainingIgnoreCase(String name);
 }
