b39d4e61166054db832fc4fdf8531753fb71b374
demian35 <josedemian@ciencias.unam.mx>
2025-05-14 16:15:53 -0600

    implementado las operaciones crud para la gestion de los tragos

--- proyectopractico/src/main/java/com/proyecto/proyectopractico/service/DrinkService.java
+++ proyectopractico/src/main/java/com/proyecto/proyectopractico/service/DrinkService.java
@@ -1,5 +1,40 @@
 package com.proyecto.proyectopractico.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.proyectopractico.model.Drink;
+import com.proyecto.proyectopractico.repository.DrinkRepository;
+
+
+
+//clase servicio que efectuara la logica para listar las bebidas, guardarlas y consultarlas
+@Service
 public class DrinkService {
     
+    @Autowired
+    private DrinkRepository drinkrepository;
+
+    //listamos todos los tragos
+    public List<Drink> getAllDrinks(){
+        return drinkrepository.findAll();
+    }
+
+    //obtenemos un nuevo trago por su id
+    public Optional getDrinkbyID(Long id){
+        return drinkrepository.findById(id);
+    }
+
+    //guardamos un trago nuevo
+    public Drink saveDrink(Drink drink){
+        return drinkrepository.save(drink);
+    }
+
+    //eliminamos el trago
+    public void deleteDrink(Long id){
+        drinkrepository.deleteById(id);
+    }
 }
