dc0c587cab574f6cb7419bc6362d30cafb62d738
demian35 <josedemian@ciencias.unam.mx>
2025-05-14 18:32:57 -0600

    metodo para redireccionar cuando se haga una insercion de una bebida y una busqueda

--- proyectopractico/src/main/java/com/proyecto/proyectopractico/controller/DrinkController.java
+++ proyectopractico/src/main/java/com/proyecto/proyectopractico/controller/DrinkController.java
@@ -1,5 +1,7 @@
 package com.proyecto.proyectopractico.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.ui.Model;
 import com.proyecto.proyectopractico.model.Drink;
 import com.proyecto.proyectopractico.service.DrinkService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -21,7 +24,27 @@ public class DrinkController {
     public String index(Model model) {
         model.addAttribute("drink", new Drink());//para el formulario
         model.addAttribute("drinks", drinkService.getAllDrinks());
-        return "index";
+        return "index";//mostramos la vista principal
     }
+
+    //endpoint para agregar bebidas
+    @GetMapping("/add")
+    public String addDrink(@ModelAttribute Drink drink) {
+        drinkService.saveDrink(drink);
+        return "redirect:/";//al agregar redirige al inicio 
+    }
+
+    //endpoint para realizar la busqueda de una bebida
+    @GetMapping("/search")
+    public String searchDrink(@RequestParam("name") String name, Model model) {
+        List<Drink> resultados= drinkService.searchDrinkbyName(name);
+        model.addAttribute("drinks", resultados);
+        model.addAttribute("drink", new Drink());//por si se quiere registrar otra nueva
+        return "index"; //mostramos en la vista principal las bebidas solicitadas
+    }
+    
+
+
+    
     
 }
